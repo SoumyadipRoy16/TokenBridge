457e5a445e2ad4fea964cbb9bb981e43305b4e97
Soumyadip Roy <roysoumyadip334@gmail.com>
2024-11-05 10:52:07 +0530

    Add files via upload

---
+++ TokenBridge/my-token-dapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TokenDApp from "./TokenDApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TokenDApp />
+    </div>
+  );
+}
+
+export default App;
